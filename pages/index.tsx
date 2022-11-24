10e94ec8c0b950b0d1d796e4a0fb8c74b378529f
Ishrak Imam <ishrak@tdf-labs.com>
2022-11-24 13:52:56 +0100

    Update query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import { JsonViewer } from '@textea/json-viewer'
 import { Loading } from '@nextui-org/react';
 
-import {create} from '@tdf-labs/the-data-faucet-sdk'
+import { create } from '@tdf-labs/the-data-faucet-sdk'
 
 const dataFaucet = create({
   url: 'https://graph.tdf-labs.io/latest/graphql',
@@ -18,28 +18,29 @@ export default function Home() {
       setData(null);
 
       const [litmusBalance, litentryBalance, erc20TokenBalance, bep20TokenBalance] = await dataFaucet.utils.compose(
-        dataFaucet.query.chain.litmus.balance({address: 'jcMqptBxZFuXpVKLmxH8wArWGxhVUo29cXNr2MmrDPTdVWfvL'}),
-        dataFaucet.query.chain.litentry.balance({address: '47BHMeKG1Q36gU6WP9ZGiqFhEPF5BhfyTVn9NSaemMd9e9uP'}),
+        dataFaucet.query.chain.litmus.balance({ address: 'jcMqptBxZFuXpVKLmxH8wArWGxhVUo29cXNr2MmrDPTdVWfvL' }),
+        dataFaucet.query.chain.litentry.balance({ address: '47BHMeKG1Q36gU6WP9ZGiqFhEPF5BhfyTVn9NSaemMd9e9uP' }),
         dataFaucet.query.chain.ethereum.erc20TokenBalance({
-          address: '0x4cfbf1cc25344ee50516cc9d2fadfe363d47e26e',
-          contract: '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+          address: '0x81d6823a1d6243bc607f390625ae3970ae86af90',
+          contract: '0xb59490aB09A0f526Cc7305822aC65f2Ab12f9723'
         }),
         dataFaucet.query.chain.bsc.bep20TokenBalance({
-          address: '0xe1e450dc0149699b0ec7b394c655866ce632a7e2',
-          contract: '0xB76da28fCCf7FeD45482954df796988E53B6169f'
+          address: '0xd03ccfe61e66112472541888485ae67425973b01',
+          contract: '0xb59490ab09a0f526cc7305822ac65f2ab12f9723'
         })
       )
 
-      setData({litmusBalance, litentryBalance, erc20TokenBalance, bep20TokenBalance});
+      console.log(JSON.stringify({ litmusBalance, litentryBalance, erc20TokenBalance, bep20TokenBalance }))
+      setData({ litmusBalance, litentryBalance, erc20TokenBalance, bep20TokenBalance });
     }
 
-    queryData().catch((error) => {console.log('Query error :::: ', error)})
+    queryData().catch((error) => { console.log('Query error :::: ', error) })
   }, [])
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        {Boolean(data) ? <JsonViewer rootName='data' enableClipboard={false} value={data}/> : <Loading />}
+        {Boolean(data) ? <JsonViewer rootName='data' enableClipboard={false} value={data} /> : <Loading />}
       </main>
     </div>
   )
